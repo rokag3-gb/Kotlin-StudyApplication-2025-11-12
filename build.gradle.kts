aae7f1d74042e9323c6ef29057abd60f92409e5a
jwoo.kim <rokag3@gmail.com>
2025-11-26 13:26:12 +0900

    REFACT: build.gradle.kts, application.yml 수정 후 정상 작동 확인

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.7"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("io.spring.dependency-management") version "1.1.7" // 의존성 버전 관리
 	kotlin("plugin.jpa") version "1.9.25"
 }
 
@@ -22,17 +22,20 @@ repositories {
 	mavenCentral()
 }
 
+// 의존성 관리
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+//	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testRuntimeOnly("com.h2database:h2")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 kotlin {
@@ -41,12 +44,17 @@ kotlin {
 	}
 }
 
+// 강의에서 추가한 내용
+// 해당 annotaion이 달린 클래스는 final -> open으로 자동 변환해라.
+// final: 상속 불가능
+// open: 상속 가능
 allOpen {
 	annotation("jakarta.persistence.Entity")
 	annotation("jakarta.persistence.MappedSuperclass")
 	annotation("jakarta.persistence.Embeddable")
 }
 
+// @Entity가 달린 클래스에 파라미터 없는 기본 생성자를 자동 생성해라.
 noArg {
     annotation("jakarta.persistence.Entity")
 }
