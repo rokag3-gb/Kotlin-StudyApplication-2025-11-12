e0dd6008eb5b440ccfdaddd084dad2bb545b273d
jwoo.kim <rokag3@gmail.com>
2025-12-23 22:43:34 +0900

    FEAT: Service, Repository layer 추가

---
+++ src/main/kotlin/com/example/study/blog/repository/repositories.kt.kt
@@ -0,0 +1,6 @@
+package com.example.study.blog.repository
+
+import com.example.study.blog.entity.Wordcount
+import org.springframework.data.repository.CrudRepository
+
+interface WorkRepository : CrudRepository<Wordcount, String>
